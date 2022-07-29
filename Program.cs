9b5819c64dad9a966a93ba77771dca9b375fb6c5
Graffchik777 <legione77@mail.ru>
2022-07-29 17:12:15 +0300

    add task9 "задолбала"

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿System.Random namberSintezator = new Random();
+
+int namber = namberSintezator.Next(10, 99);
+Console.WriteLine(namber);
+
+
+int firstNamber = namber / 10;
+int secondNamber = namber % 10;
+
+if (firstNamber > secondNamber)
+{
+    Console.WriteLine(firstNamber);
+}
+else
+{
+    Console.WriteLine(secondNamber);
+ } нарушение загрузки на github
